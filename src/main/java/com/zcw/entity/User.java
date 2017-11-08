6f227cb3c4a154c261853ff8e1c94061981b2cc8
Kikyou <chaowei110@qq.com>
2017-11-08 16:14:58 +0800

    JdbcTemplate

---
+++ src/main/java/com/zcw/entity/User.java
@@ -0,0 +1,102 @@
+package com.zcw.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "USER_T")
+public class User implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8656940771942534240L;
+
+	// @Id
+	// @Column(name = "UT_ID")
+	private Long utId;
+
+	// @Column(name = "UT_NAME")
+	private String utName;
+
+	// @Column(name = "UT_EMAIL")
+	private String utEmail;
+
+	// @Column(name = "UT_ADDR")
+	private String utAddr;
+
+	// @Column(name = "UT_GENDER")
+	private short utGender;
+
+	// @Column(name = "UT_ORGID")
+	private String utOrgid;
+
+	public User(Long utId, String utName, String utEmail, String utAddr, short utGender, String utOrgid) {
+		super();
+		this.utId = utId;
+		this.utName = utName;
+		this.utEmail = utEmail;
+		this.utAddr = utAddr;
+		this.utGender = utGender;
+		this.utOrgid = utOrgid;
+	}
+
+	public Long getUtId() {
+		return utId;
+	}
+
+	public void setUtId(Long utId) {
+		this.utId = utId;
+	}
+
+	public String getUtName() {
+		return utName;
+	}
+
+	public void setUtName(String utName) {
+		this.utName = utName;
+	}
+
+	public String getUtEmail() {
+		return utEmail;
+	}
+
+	public void setUtEmail(String utEmail) {
+		this.utEmail = utEmail;
+	}
+
+	public String getUtAddr() {
+		return utAddr;
+	}
+
+	public void setUtAddr(String utAddr) {
+		this.utAddr = utAddr;
+	}
+
+	public short getUtGender() {
+		return utGender;
+	}
+
+	public void setUtGender(short utGender) {
+		this.utGender = utGender;
+	}
+
+	public String getUtOrgid() {
+		return utOrgid;
+	}
+
+	public void setUtOrgid(String utOrgid) {
+		this.utOrgid = utOrgid;
+	}
+
+	@Override
+	public String toString() {
+		return "User [utId=" + utId + ", utName=" + utName + ", utEmail=" + utEmail + ", utAddr=" + utAddr
+				+ ", utGender=" + utGender + ", utOrgid=" + utOrgid + "]";
+	}
+
+}
--- src/test/java/com/zcw/OracleConnApplicationTests.java
+++ src/test/java/com/zcw/OracleConnApplicationTests.java
@@ -1,15 +1,24 @@
 package com.zcw;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.zcw.entity.User;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class OracleConnApplicationTests {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
@@ -30,6 +39,26 @@ public class OracleConnApplicationTests {
 	public void insertPRKIncrTest() {
 		String sql = "insert into USER_T values(seq_user.nextval,'wangwu','wangwu@qq.com','wuhan',0,'sanxin')";
 		jdbcTemplate.execute(sql);
+	}
+
+	@Test
+	public void inserBatch() {
+
+	}
+
+	@Test
+	public void oracleFunction() {
+		String sql = "select LENGTH(t.UT_NAME) as length from USER_T t where UT_ID = 5";
+		Integer length = jdbcTemplate.queryForObject(sql, Integer.class);
+		logger.info("length = {}", String.valueOf(length));
+
+		sql = "select UT_ID, UT_NAME, UT_EMAIL, UT_ADDR, UT_GENDER, UT_ORGID from USER_T t";
+		List<User> userList = jdbcTemplate.query(sql, new Object[] {},
+				(rs, rowNum) -> new User(rs.getLong("UT_ID"), rs.getString("UT_NAME"), rs.getString("UT_EMAIL"),
+						rs.getString("UT_ADDR"), rs.getShort("UT_GENDER"), rs.getString("UT_ORGID")));
+		for (User user : userList) {
+			logger.info(user.toString());
+		}
 	}
 
 }
